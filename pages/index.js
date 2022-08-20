1458ec5e9bdf8c2d8789d92f145b81545f372693
Jacob Habib <habib.50@osu.edu>
2022-08-20 18:04:05 -0500

    Added footer component to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
@@ -14,7 +15,7 @@ export default function Home() {
                 body component goes here
             </main>
             <footer>
-                footer component goes here
+                <Footer/>
             </footer>
         </div>
     )
