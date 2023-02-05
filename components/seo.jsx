a5da39b20b0e969e218dfc8d3e777714a81ebc71
Lucas Sierota <lucasemanuelss@hotmail.com>
2023-02-05 20:52:19 -0300

    fix: favicon sizes and target all platforms

--- components/seo.jsx
+++ components/seo.jsx
@@ -36,8 +36,25 @@ const SEO = (props) => {
       {/* <html lang="en" /> */}
       {props.children}
       <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
         rel="icon"
-        href="./icon.svg"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="manifest"
+        href="/site.webmanifest"
       />
       {title && buildMetaTitle(title)}
       {description &&
@@ -47,10 +64,6 @@ const SEO = (props) => {
       <link
         rel="canonical"
         href={url}
-      />
-      <link
-        rel="icon"
-        href="./icon.svg"
       />
       {/* FACEBOOK */}
 
