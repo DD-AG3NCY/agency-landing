c9d92904e54220b991f7a5dbfc36a276e99667ef
Gordo <60603572+gordo-labs@users.noreply.github.com>
2023-10-13 12:14:28 +0200

    rebase main

--- .vercel/output/static/_next/static/Ko3sZBfTVR2RQm8XPkocq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/65-e09d89dad1c8358f.js","static/chunks/pages/index-421d9a9ae02ab27e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
