e3b49580568f798644d78d9ab4bc067e86ca05d5
Gordo <60603572+gordo-labs@users.noreply.github.com>
2023-10-13 16:07:11 +0200

    css not building correctly at ds

--- .vercel/output/static/_next/static/iasTGIznQQTmXVSTfLKG8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/717-5c98d28b047bdb8c.js","static/chunks/pages/index-f6228dd724a002fd.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
